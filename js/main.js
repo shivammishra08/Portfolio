105dfe4cab3071bd0379aaa70cbfb750e59ea0e4
shivammishra08 <shivam21mishra08@gmail.com>
2023-07-02 00:05:06 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.querySelector(".navbar");
     const sections = document.querySelectorAll("section");
     const logoSpan = navbar.querySelector(".navbar .logo a span");
-  
+    const footer = document.querySelector("footer");
     function handleNavbarAppearance() {
       const currentSection = Array.from(sections).find(section => {
         const rect = section.getBoundingClientRect();
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
       if (currentSection) {
         navbar.className = "navbar"; // Reset the class to remove all previous styles
-  
+        footer.className = "footer";
         if (currentSection.id === "home") {
           navbar.classList.add("blur-navbar");
           logoSpan.style.color = "#00bcd4";
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
       if (window.scrollY === 0) {
         navbar.classList.remove("blur-navbar");
+        footer.classList.add("hidden")
       }
     }
   
@@ -91,10 +92,37 @@ function reveal(){
       }
     }
   }
+
+  var map = L.map('map').setView([37.7749, -122.4194], 12);
+
+  // Add the OpenStreetMap tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  
+  // Create a marker and add it to the map
+  L.marker([37.7749, -122.4194]).addTo(map)
+    .bindPopup('Your Desired Location')
+    .openPopup();
+    
+// send email
+function sendMail(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+  
+    Email.send({
+      SecureToken: "09b84e2e-e900-411d-ba95-8b160630be54",
+      To: '20051028@kiit.ac.in',
+      From: document.getElementById('email').value,
+      Subject: "New Message from " + document.getElementById('name').value,
+      Body: "Name: " + document.getElementById('name').value + "<br>Email: " + document.getElementById('email').value + "<br>Message: " + document.getElementById('message').value
+    }).then(function () {
+      alert("Mail Sent Successfully");
+      // Reset the form fields after sending the email
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+    });
+  }
   
 
 
-
-
-
-  
